e09a4b3276f23422780ea899fa8ee6266c96c3cf
`AbdulJuste` <jusabd04@ecolecatholique.ca>
2022-10-21 14:05:48 -0400

    almost done

---
+++ Bonus.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bonus {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String caractère;
+        int nombre;
+
+        System.out.println("Choisir un caractère");
+        caractère = keyboard.nextLine();
+
+        System.out.println("Choisir un nombre");
+        nombre = keyboard.nextInt();
+
+        System.out.println()
+
+
+
+        
+    }
+}
