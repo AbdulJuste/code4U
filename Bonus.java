a39c3645e2e4b52a1d585fb1cbf7e57d87a45b1f
`AbdulJuste` <jusabd04@ecolecatholique.ca>
2022-10-25 10:28:24 -0400

    1

--- Bonus.java
+++ Bonus.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Bonus {
-    public static void main(String[] args) {
+class Bonus {
+    static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         String caractère;
         int nombre;
@@ -12,10 +12,9 @@ public class Bonus {
         System.out.println("Choisir un nombre");
         nombre = keyboard.nextInt();
 
-        System.out.println()
-
-
-
+        System.out.println( "Vous avez choisi:"  + caractère + nombre );
+    }
+    
         
     }
 }
